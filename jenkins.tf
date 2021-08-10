616c1654f17dcce9fd29f8438edc3566a965612a
pjagannadha4 <86137800+pjagannadha4@users.noreply.github.com>
2021-08-10 20:44:27 +0530

    update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  access_key = "AKIASERCLNJTGQI3I4SX"
-  secret_key = "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"
-  region     = "ap-south-1"
-}
 
 # data "aws_vpc" "selected" {
 #   id = "aws_vpc.terraform-vpc.id"
